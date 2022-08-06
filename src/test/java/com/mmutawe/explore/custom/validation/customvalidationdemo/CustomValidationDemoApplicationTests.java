858fbe9ba037945bbce52d0cd834d1a168092f8b
mmutawe <mahmoudmutawe@gmail.com>
2022-08-06 14:45:56 -0500

    Initial commit

---
+++ src/test/java/com/mmutawe/explore/custom/validation/customvalidationdemo/CustomValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmutawe.explore.custom.validation.customvalidationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomValidationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
